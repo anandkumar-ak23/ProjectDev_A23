40ce50543f28f9deb3a3f9f0f91147a0c3fed133
AdmiN <AdmiN@ITWS0032>
2024-08-29 10:46:02 +0530

    first commit by v

--- ProjectDevA23/src/package1_a23/Class_ak_1.java
+++ ProjectDevA23/src/package1_a23/Class_ak_1.java
@@ -5,6 +5,9 @@ public class Class_ak_1 {
 	public int addition(int a,int b) {
 		return a+b;
 	}
+	public int sub(int a,int b) {
+		return a+b;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Arthemitic operations");
