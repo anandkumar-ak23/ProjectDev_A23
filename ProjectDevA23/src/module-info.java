ba6254d8147224ade57a8196e6405321b9e1e8d9
AdmiN <AdmiN@ITWS0031>
2024-08-29 10:25:33 +0530

    First commit by ak

---
+++ ProjectDevA23/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProjectDevA23 {
+}
---
+++ ProjectDevA23/src/package1_a23/Class_ak_1.java
@@ -0,0 +1,12 @@
+package package1_a23;
+
+public class Class_ak_1 {
+	
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Arthemitic operations");
+	}
+}
